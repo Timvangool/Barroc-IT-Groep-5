5402ddadd0006583df808fb8e4f38e4c2a3af4bc
xSvenHD <xsvenhd@hotmail.com>
2014-10-29 16:11:16 +0100

    Revert "update"
    This reverts commit d850f45096eb8c9b59e67294b0ca0f5b23d317e6.

--- Barroc-IT Programma/Barroc-IT Programma/Program.cs
+++ Barroc-IT Programma/Barroc-IT Programma/Program.cs
@@ -16,7 +16,7 @@ namespace Barroc_IT_Programma
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Launcher());
+            Application.Run(new frmLogin());
         }
     }
 }
--- Barroc-IT Programma/Barroc-IT Programma/frmMenu.cs
+++ Barroc-IT Programma/Barroc-IT Programma/frmMenu.cs
@@ -10,7 +10,6 @@ using System.Windows.Forms;
 using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
-using System.Data.SqlServerCe;
 
 namespace Barroc_IT_Programma
 {
@@ -21,49 +20,25 @@ namespace Barroc_IT_Programma
         {
             InitializeComponent();
             dbh = new DatabaseHandler();
+            permissions();
+
+            
         }
-        public bool permissions()
+        public void permissions()
         {
-            dbh.TestConnection();
-            dbh.OpenCon();
             bool admin = false;
-            string user = lbl_user.Text;
-            int permissionValue = 0;
-            string sql = "SELECT * FROM  tbl_Account WHERE Username = @user";
-            SqlCommand cmd = new SqlCommand(sql, dbh.Getcon());
-            cmd.Parameters.Add(new SqlParameter("user", user));
-            SqlDataReader rdr = cmd.ExecuteReader();
-            while (rdr.Read())
+            using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM [Table] WHERE Permisions = 4", dbh.Getcon()))
             {
-                permissionValue = rdr.GetInt32(3);
+                admin = (int)cmd.ExecuteScalar() > 0;
             }
-            if (permissionValue == 4)
+          
+            if (admin)
             {
-                admin = true;
-            }
-            dbh.CloseCon();
-            return admin;
-        }
-
-        private void btnAdminPanel_Click(object sender, EventArgs e)
-        {
-           bool admin = permissions();
-
-            if(admin)
-            {
-                // Open admin panel
+                
             }
             else
             {
-                MessageBox.Show("You don't have permissions to open the Admin Panel");
             }
-        }
-
-        private void btnLogout_Click(object sender, EventArgs e)
-        {
-            frmLogin loginFrm = new frmLogin();
-            this.Hide();
-            loginFrm.Show();
         }
     }
 }
